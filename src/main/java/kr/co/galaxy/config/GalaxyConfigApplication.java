07c0ec2f448f05222a44b985b995ad5750699eeb
kst <kst@osstem.com>
2024-03-07 18:56:59 +0900

    fix : spring cloud config server 초기화

---
+++ src/main/java/kr/co/galaxy/config/GalaxyConfigApplication.java
@@ -0,0 +1,13 @@
+package kr.co.galaxy.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyConfigApplication.class, args);
+	}
+
+}
